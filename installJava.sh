8da702a5a6b8ab052cc41fff1c4a10b434aea301
javier-a-paredes <javi4202@gmail.com>
2024-04-21 23:36:50 -0300

    maven clean + script

--- installJava.sh
+++ installJava.sh
@@ -7,25 +7,32 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install unzip
 
 # Chrome-For-Testing download link | version 123.0.6312.122
-wget https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.122/linux64/chrome-headless-shell-linux64.zip
-
+wget https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.122/linux64/chrome-linux64.zip
+# headless: wget https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.122/linux64/chrome-headless-shell-linux64.zip
 # Unzip
 unzip chrome-linux64.zip
+#headless unzip chrome-headless-shell-linux64.zip
 
 # Cleanup
 rm chrome-linux64.zip
+#headless rm chrome-headless-shell-linux64.zip
 
 # Download chromedriver
-wget 
+wget https://storage.googleapis.com/chrome-for-testing-public/123.0.6312.122/linux64/chromedriver-linux64.zip
+
+unzip chromedriver-linux64.zip
+mv chromedriver-linux64/chromedriver ./src/main/resources/chromedriver
+
+# Cleanup
+rm chromedriver-linux64.zip
 
 # Install Java 18
-sudo apt install default-jre
+sudo apt install -y default-jre 
 sudo apt-get install -y openjdk-18-jdk
 
 # verify java
 java -version
 javac -version
-
 
 # Install Maven Apache
 sudo apt install maven -y
